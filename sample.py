7f55f4851378f5a449b6685e4ec3c34f79a49506
kojima kentaro <kentaroukojiken@gmail.com>
2021-06-21 13:25:08 +0900

    main

---
+++ sample.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+#python sample.py
+
+
+"""def hello(name):
+
+    message = "{0}さん、こんにちは".format(name)
+    print(message)
+
+hello("kojima")
+
+"""
+
+def check_name(name):
+    if len(name) >=3:
+        return True
+    else:
+        return False
+
+
+name = input()
+result = check_name(name)
+if result:
+    print("登録完了")
+else:
+    print("名前が短いです")
+
+    
