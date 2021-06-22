a67ea123785ed313986275528c97ef0920c98379
kojima kentaro <kentaroukojiken@gmail.com>
2021-06-22 10:59:34 +0900

    main

--- c.c
+++ c.c
@@ -27,3 +27,24 @@ int main(void){
 
 */
 
+
+void func(int *a, int *b){
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+
+}
+
+int main(void){
+    int i,j,array[5] = {3,1,5,2,4};
+    for(i = 0;i < 4; i++)
+        for(j = i + 1; j < 5; j++)
+            if(array[i] > array[j])
+                func(array + i,array + j);
+    
+    for(i = 0;i < 5; i++)
+        printf("%d",array[i]);
+
+    printf("\n");
+
+}
